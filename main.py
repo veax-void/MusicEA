cd3f26176f46a822a2fc76faa63f922114de528f
nightstorm0909 <nilotpalsinha1990@gmail.com>
2020-05-18 15:02:20 +0800

    data option added to main

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ Created on Mon May 18 13:41:59 2020
 """
 from midi_utils import midi_to_csv, csv_to_notes, notes_to_midi
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser("cifar")
+parser.add_argument('--data', type=str, default=None, help='location of midi file')
+args = parser.parse_args()
 
 def main():
-    midi_filename = 'bwv988.mid'
-    
-    csv_midi = midi_to_csv(midi_filename)
-    print(csv_midi.shape, csv_midi, '\n')
+	if args.data is not None:
+		midi_filename = args.data
+	
+	csv_midi = midi_to_csv(midi_filename)
+	print(csv_midi.shape, csv_midi, '\n')
 
-    notes = csv_to_notes(csv_midi)
-    print(notes.shape, notes, '\n')
-    
-    new_mini_name = 'ten_notes.mid'
-    ten_notes = np.zeros(notes.shape, dtype='int') + 10
-    ten_notes = np.array(ten_notes, dtype='str')
-    
-    midi_file = notes_to_midi(midi_filename, ten_notes, new_mini_name)
-    
-    
+	notes = csv_to_notes(csv_midi)
+	print(notes.shape, notes, '\n')
+	
+	new_mini_name = 'ten_notes.mid'
+	ten_notes = np.zeros(notes.shape, dtype='int') + 10
+	ten_notes = np.array(ten_notes, dtype='str')
+	
+	midi_file = notes_to_midi(midi_filename, ten_notes, new_mini_name)
+	
+	
 if __name__ == '__main__':
-    main()
+	main()
